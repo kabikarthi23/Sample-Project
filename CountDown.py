08380e10ffabb867649d7a2faa2804ce72f32ff6
kabikarthi23 <kabikarthi23@gmail.com>
2024-03-09 21:21:34 +0530

    CountDown.py file added

--- CountDown.py
+++ CountDown.py
@@ -1,1 +1,18 @@
 #Creating a python program on Countdown
+
+#importing requiring libraries
+import time
+
+#creating the function
+def CountDown(time_sec):
+    while time_sec:
+        min,sec = divmod(time_sec,60)
+        timecount = "{:02d} : {:02d}".format(min,sec)
+        print(timecount)
+        time.sleep(1)
+        time_sec -= 1
+
+    print("Let's Start")
+
+#calling the function
+CountDown(10)
